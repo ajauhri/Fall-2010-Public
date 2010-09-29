bdec474f5438d3c083db441f8f69bb762d2276a2
Asem Radhwi <asem.radhwi@sv.cmu.edu>
2010-09-29 12:18:34 -0700

    Added user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ describe User do
       :password => "12345678",
       :password_confirmation => "12345678",
       :email => "newuser@newuser.com",
-     }
+    }
   end
 
   it "should create a new instance given valid attributes" do
@@ -16,11 +16,31 @@ describe User do
   end
 
   it "should succeed creating a new :valid_user from the Factory" do
-  Factory.create(:valid_user)
-end
+    Factory.create(:valid_user)
+  end
 
-it "should invalid :invalid_user factory" do
-  Factory.build(:invalid_user).should be_invalid
-end
+  it "should invalid :invalid_user factory" do
+    Factory.build(:invalid_user).should be_invalid
+  end
+
+  it "should invalid :invalid_email factory" do
+    Factory.build(:invalid_email).should be_invalid
+  end
+
+  it "should invalid :preexisting_username factory" do
+    Factory.build(:preexisting_username).should be_invalid
+  end
+
+  it "should invalid :preexisting_email factory" do
+    Factory.build(:preexisting_email).should be_invalid
+  end
+
+  it "should invalid :nonmatching_password factory" do
+    Factory.build(:nonmatching_password).should be_invalid
+  end
+
+  it "should invalid :invalid_username factory" do
+    Factory.build(:invalid_username).should be_invalid
+  end
 
 end
