e1824ec8c997232014cc9e7d2eb3bf03fdefd762
Asem Radhwi <asem.radhwi@sv.cmu.edu>
2010-09-30 18:03:51 -0700

    Changed the layout of the page

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100927002923) do
+ActiveRecord::Schema.define(:version => 20100930231425) do
 
   create_table "lifecycles", :force => true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20100927002923) do
     t.string   "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
 end
