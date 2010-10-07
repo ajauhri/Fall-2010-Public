1774519582be2820e766f972eca3f814fe6e0bc3
intisoto <inti.soto@sv.cmu.edu>
2010-10-07 10:19:36 -0700

    Added relationship from ProjectPhase to Deliverable

--- app/models/project_phase.rb
+++ app/models/project_phase.rb
@@ -1,6 +1,7 @@
 class ProjectPhase < ActiveRecord::Base
   belongs_to :project
   belongs_to :lifecycle_phase
+  has_many :deliverables
 
   def self.create_from_lifecycle_phase(lifecycle_phase_id, project)
     @lifecycle_phase = LifecyclePhase.find(lifecycle_phase_id)
