c9d22d29246726d2b13a87f1a6a900515c29eb37
Abhinav Jauhri <aj910martin@gmail.com>
2010-10-24 16:36:31 -0700

    estimate deliverable TDD, tests not working.

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -9,25 +9,24 @@ describe "/projects/show.html.erb" do
     login_user
     @lifecycle = Factory.create(:lifecycle)
     @project_phases = []
-    iteration0 = Factory.create(:xp_iteration0)
-    @project_phases << iteration0
+    @iteration0 = Factory.create(:xp_iteration0)
+    #@project_phases << iteration0
   end
 
- # before(:each) do
- #   @project = stub(Project,
- #     :name => "Project 1",
- #     :description => "value for description",
- #     :sequence => 1,
- #     :lifecycle_id => @lifecycle.id)
- # end
+ before(:each) do
+   @project = Factory.create(:project)
+ end
 
 
   it "renders attributes in <ul>" do
-    render :partial => "/phases/project_phase", :locals => {:project_phases => @project_phases}
-    response.should have_tag('ul') do
-      with_tag('li', 'Iteration Planning Meeting Minutes')
-    end
-
+    render :partial => "/project_phases/project_phase", :locals => {:project_phase => @iteration0}
+    get "/projects/show/#{@project.id}"
+    response.should render_template('show')
+    response.should have_tag('table') 
+    # do
+    #       with_tag('li', 'Iteration Planning Meeting Minutes')
+    #     end
+    
   end
 end
 
