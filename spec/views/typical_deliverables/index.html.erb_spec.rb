dcd76d355b30807e5c74d10cd2213cc078320b96
Asem Radhwi <asem.radhwi@sv.cmu.edu>
2010-10-28 14:56:25 -0700

    Written the test for the index.html view of typical_deliverables

--- spec/views/typical_deliverables/index.html.erb_spec.rb
+++ spec/views/typical_deliverables/index.html.erb_spec.rb
@@ -4,11 +4,18 @@ describe "/typical_deliverables/index.html.erb" do
   include TypicalDeliverablesHelper
 
   before(:each) do
-    assigns[:typical_deliverables] = [
-      stub_model(TypicalDeliverable),
-      stub_model(TypicalDeliverable)
-    ]
+    expected_typical_deliverables = [ stub_model(TypicalDeliverable), stub_model(TypicalDeliverable)]
+
+    expected_typical_deliverables.each do |typical_deliverable|
+      typical_deliverable.stub(:get_production_rate)
+      typical_deliverable.stub(:deliverable_type).and_return(stub_model(DeliverableType))
+      typical_deliverable.stub(:lifecycle_phase).and_return(stub_model(LifecyclePhase))
+
+    end
+    assigns[:typical_deliverables] = expected_typical_deliverables
+
   end
+
 
   it "renders a list of typical_deliverables" do
     render
