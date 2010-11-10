692da5c8676301bee374704a87ecb7a4f23586f5
Asem Radhwi <asem.radhwi@sv.cmu.edu>
2010-11-10 12:08:47 -0800

    Fixed the deliverables test (added estimated_production_rate)

--- spec/factories/deliverables.rb
+++ spec/factories/deliverables.rb
@@ -5,6 +5,7 @@ Factory.define :xp_deliverable1, :class => Deliverable do |p|
   p.complexity "LOW"
   p.estimated_size 3
   p.estimated_effort 3
+  p.estimated_production_rate 2
 
   # This uses association and :lifecycle factory
   p.project_phase_id {|l| l.association(:xp_iteration0) }
@@ -20,6 +21,7 @@ Factory.define :low_complex_minutes1, :class => Deliverable do |p|
   p.actual_production_rate 1
   p.estimated_size 3
   p.estimated_effort 3
+  p.estimated_production_rate 2
 end
 
 Factory.define :low_complex_minutes2, :class => Deliverable do |p|
@@ -32,6 +34,7 @@ Factory.define :low_complex_minutes2, :class => Deliverable do |p|
   p.actual_production_rate 3
   p.estimated_size 3
   p.estimated_effort 3
+  p.estimated_production_rate 2
 end
 
 Factory.define :low_complex_minutes3, :class => Deliverable do |p|
@@ -44,6 +47,7 @@ Factory.define :low_complex_minutes3, :class => Deliverable do |p|
   p.actual_production_rate 5
   p.estimated_size 3
   p.estimated_effort 3
+  p.estimated_production_rate 2
 end
 
 
@@ -57,6 +61,7 @@ Factory.define :high_complex_requirements1, :class => Deliverable do |p|
   p.actual_production_rate 2
   p.estimated_size 3
   p.estimated_effort 3
+  p.estimated_production_rate 2
 end
 
 Factory.define :high_complex_requirements2, :class => Deliverable do |p|
@@ -69,6 +74,7 @@ Factory.define :high_complex_requirements2, :class => Deliverable do |p|
   p.actual_production_rate 4
   p.estimated_size 3
   p.estimated_effort 3
+  p.estimated_production_rate 2
 end
 
 Factory.define :high_complex_requirements3, :class => Deliverable do |p|
@@ -81,4 +87,27 @@ Factory.define :high_complex_requirements3, :class => Deliverable do |p|
   p.actual_production_rate 6
   p.estimated_size 3
   p.estimated_effort 3
+  p.estimated_production_rate 2
+end
+
+Factory.define :effort_deliverable, :class => Deliverable do |p|
+  p.name "Document"
+  p.deliverable_type "Document"
+  p.unit_of_measure "Pages"
+  p.complexity "LOW"
+  p.estimated_size 3
+  p.estimated_effort 3
+  p.estimated_production_rate 2
+  p.project_phase {|l| l.association(:effort_project_phase) }
+end
+
+Factory.define :effort_deliverable2, :class => Deliverable do |p|
+  p.name "Document2"
+  p.deliverable_type "Document2"
+  p.unit_of_measure "Pages"
+  p.complexity "LOW"
+  p.estimated_size 3
+  p.estimated_effort 30
+  p.estimated_production_rate 2
+  p.project_phase {|l| l.association(:effort_project_phase) }
 end
